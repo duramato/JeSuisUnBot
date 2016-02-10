6b864f7be9fe8801ac4000e4faa40cef8f3afad7
supergonkas <supergonkas@gmail.com>
2016-02-10 18:05:06 +0000

    More changes

--- ircbot/config.py
+++ ircbot/config.py
@@ -8,72 +8,49 @@ import ConfigParser
 config = ConfigParser.RawConfigParser()
 config.read('config.cfg')
 
-# some commands can be executed only if the user's nick is found in this list
 owner = list(set([
-    'supergonkas',
+    'ownernick',
 ]))
 
 owner_email = {
-    'supergonkas': 'goncalo-matos@hotmail.com',
+    'owner': 'owneremail',
 }
 
-# server to connect to
+
 server = config.get('network', 'server')
-# server's port
+
 port = int(config.get('network', 'port'))
 
-# bot's nicknames
 nicks = map(lambda s: s.strip('\''), config.get('network', 'nicknames').split(','))
-# bot's real name
+
 real_name = config.get('network', 'real_name')
 
-# channels to join on startup
 channels = map(lambda s: s.strip('\''), config.get('network', 'channels').split(','))
 
 cmds = {
-    # core commands list, these commands will be run in the same thread as the bot
-    # and will have acces to the socket that the bot uses
+
     'core': list(set([
         'quit',
         'join',
         'channels',
     ])),
 
-    # normal commands list, the ones that are accessible to any user
     'user': list(set([
-        'task',
-        'wikipedia',
-        'wiki',
-        'answer',
-        'weather',
-        'uptime',
-        'google',
-        'tvdb',
-        'uptime',
-        'issues',
-        'issue',
-        'join',
-        'quit',
-        'about',
-        'shows',
-        'hash',
-        'torrent',
-        'ssl',
+        'plugin1',
+        'plugin2',
+        'plugin3',
     ])),
 
-    # commands list that the bot will execute even if a human didn't request an
-    # action
+
     'auto': list(set([
     ])),
 }
 
-# smtp server for email_alert
-smtp_server = 'smtp.gmail.com'
-smtp_port = 25
-from_email_address = 'changeme@gmail.com'
-from_email_password = 'p@s$w0rd'
+smtp_server = 'server'
+smtp_port = port
+from_email_address = 'adress'
+from_email_password = 'pass'
 
-# users should NOT modify below!
 log = os.path.join(os.getcwd(), 'logs', '')
 logging_level = logging.DEBUG
 start_time = time.time()
